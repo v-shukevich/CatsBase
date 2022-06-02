2a47f9f56f6909a019e4258b2f0cbd3516c4b74d
Vitali Shukevich <v.shukevich@iomico.com>
2022-06-02 22:04:52 +0300

    init commit

---
+++ app/src/main/java/com/shukevich/catsbase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shukevich.catsbase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
